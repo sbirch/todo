9271299165b8a9f0593fe3bc2f0b221cf963a9eb
Sam Birch <sam.m.birch@gmail.com>
2013-08-20 15:16:39 -0700

    updated URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='todo',
       version='0.1',
       description='Print TODOs in a codebase',
-      url='',
+      url='https://github.com/sbirch/todo',
       author='Sam Birch',
       author_email='sam.m.birch@gmail.com',
       license='MIT',
